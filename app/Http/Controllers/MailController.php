d85ca904d98541c35ff6e30de41b99f8c3b8accb
Musa Skosana <mskosana@twkagri.com>
2021-11-05 20:18:40 +0200

    Called the api to get employees

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -5,10 +5,22 @@ namespace App\Http\Controllers;
 use App\Mail\SendMail;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
+
+    public function index()
+    {
+        $todayDate = date('Y-m-d');
+
+        $api_url = "https://interview-assessment-1.realmdigital.co.za/employees";
+
+        $employees = Http::get($api_url)->json();
+
+        dd($employees);
+    }
 
     public function sendMail()
     {
@@ -17,7 +29,7 @@ class MailController extends Controller
             'body' => 'This is for testing mail using gmail.'
         ];
 
-        Mail::to("umswa1994@gmail.com")->send(new SendMail($data));
+        Mail::to("umswaonetjhejo@gmail.com")->send(new SendMail($data));
 
         return "Basic Email Sent. Check your inbox.";
     }
